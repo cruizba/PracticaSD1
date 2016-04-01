53e356f8f11891fffc0eff5d957a62ed188283f3
Carlos Ruiz Ballesteros <c.ruizbal94@gmail.com>
2016-04-01 18:44:49 +0200

    Añadidas las opciones de añadir modificar y eliminar contratados

--- src/main/java/SD/Practica_1/Controller/ContratadosController.java
+++ src/main/java/SD/Practica_1/Controller/ContratadosController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,6 +27,56 @@ public class ContratadosController {
 		model.addAttribute("contratados", listCon);
 		
 		return "personal";
+	}
+	
+	@RequestMapping(value="/insertarPersonal", method=RequestMethod.GET)
+	public String insertarPersonal(){
+		
+		return "insertarPersonal";
+	}
+	
+	@RequestMapping("/insertarPersonalBD")
+	public String insertar(Contratado contratado, Model model) {
+
+		conRep.save(contratado);
+
+		return "insercionExito";
+	}
+	
+	@RequestMapping("/modificarPersonal/{num}")
+	public String modificarPersonal(@PathVariable(value = "num") Integer num, Model model){
+		
+		Contratado contratado = conRep.findOne(num);
+		
+		model.addAttribute(contratado);
+		
+		return "modificarPersonal";
+		
+	}
+	
+	@RequestMapping("/modificarPersonalBD/{num}")
+	public String modificar(@PathVariable(value = "num") Integer num, Contratado contratado, Model model){
+		
+		Contratado contratadoAux = conRep.findOne(num);
+		
+		contratadoAux.setNombre(contratado.getNombre());
+		contratadoAux.setApellidos(contratado.getApellidos());
+		contratadoAux.setTelFijo(contratado.getTelFijo());
+		contratadoAux.setTelMovil(contratado.getTelMovil());
+		contratadoAux.setTipo(contratado.getTipo());
+		
+		conRep.save(contratadoAux);
+		
+		return "edicionExito";
+	}
+	
+	@RequestMapping("/eliminarPersonalBD/{num}")
+	public String eliminar(@PathVariable(value="num") Integer num, Model model){
+		
+		conRep.delete(num);
+		
+		return "borradoExito";
+		
 	}
 	
 }
